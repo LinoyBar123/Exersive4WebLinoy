c95290aad15a16c664c99ab5abe2d3622539dfa0
Linoybar123 <Linoybar15@gmail.com>
2024-07-20 23:14:19 +0300

    remove lines empty

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,6 @@ const db = require('../db');
 exports.registerUser = (req, res) => {
     console.log(db);
     const { username, password, accessCode } = req.body;
-
     const sql = 'INSERT INTO tbl_100_users (username, password, accessCode) VALUES (?, ?, ?)';
     db.query(sql, [username, password, accessCode], (err, results) => {
         if (err) {
@@ -15,18 +14,15 @@ exports.registerUser = (req, res) => {
 
 exports.loginUser = (req, res) => {
     const { username, password } = req.body;
-
     const sql = 'SELECT * FROM tbl_100_users WHERE username = ?';
     db.query(sql, [username], (err, results) => {
         if (err || results.length === 0) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
-        
         const user = results[0];
         if (user.password !== password) {  
             return res.status(401).json({ message: 'Invalid credentials' });
         }
-        
         res.status(200).json({ message: 'Login successful', accessCode: user.accessCode });
     });
 };
